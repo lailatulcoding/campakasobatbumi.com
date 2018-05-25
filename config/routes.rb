ec398a9d98811a711d02158c038fae470e54e51a
Radiet <radiet@41studio.com>
2018-05-26 00:11:27 +0700

    Init assets migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
+  root 'pages#landing'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
     registrations:  'api/v1/registrations'
