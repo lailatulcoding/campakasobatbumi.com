7b91d6e23e796377362a7cf11601679ecc9b1bb1
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2018-05-25 23:42:47 +0700

    integrate rails admin with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  apipie
 end
