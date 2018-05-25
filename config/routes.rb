3558c97cc1b24eea069add24b35c9f0a3dd41253
Radiet <radiet@41studio.com>
2018-05-26 02:04:49 +0700

    Add products routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     # sessions:  'api/v1/sessions',
     # passwords:  'api/v1/passwords'
   }
-
+  resources :products, only: :index
   namespace :api, defaults: { format: :json }  do
     namespace :v1 do
       # get    'versions/state'       => 'versions#state'
